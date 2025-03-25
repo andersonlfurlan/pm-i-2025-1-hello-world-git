f1592d867ccd31fba93a7d5059409e37c4ee9e07
Anderson Luís Furlan <alsfurlan@gmail.com>
2025-03-24 21:19:24 -0300

    Adicionada funcionalidade de limpar (#5)

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,28 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
+const limparButton = document.getElementById('limpar');
+
+const limpar = () => {
+    nameInput.value = '';
+    messageP.innerText = '';
+}
+
+limparButton.addEventListener('click', limpar);
 
 nameInput.onkeyup = (event) => {
+
+    if (event.code === 'Enter') {
+        helloWorld();
+    }
+
+    if (event.code === 'Backquote') {
+        limpar();
+    }
+
     if (nameInput.value === '') {
         messageP.innerText = '';
     }
 }
-
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
 }
